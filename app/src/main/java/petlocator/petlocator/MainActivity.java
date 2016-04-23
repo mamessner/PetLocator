9b979ffae489ef5eeb2875eb87c811e9e63a5b06
mamessner <mamessner@email.wm.edu>
2016-04-22 23:59:23 -0400

    Added an action bar and navigation button to the top of the screen.

--- app/src/main/java/petlocator/petlocator/MainActivity.java
+++ app/src/main/java/petlocator/petlocator/MainActivity.java
@@ -1,8 +1,12 @@
 package petlocator.petlocator;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -10,12 +14,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar actionBar = (Toolbar) findViewById(R.id.action_bar);
+        Drawable menuButton = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.menu_button, null);
+        actionBar.setNavigationIcon(menuButton);
+        setSupportActionBar(actionBar);
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -25,6 +34,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
                 .title("Marker"));
-        map.setMyLocationEnabled(true);
+        try {
+            map.setMyLocationEnabled(true);
+        } catch (SecurityException e) {
+            // TODO: do something
+        }
     }
 }
