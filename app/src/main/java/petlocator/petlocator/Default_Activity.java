9e228ce1acb70f7868e9efbec0039e937ccace9c
maxmiroff <maxmiroff@gmail.com>
2016-04-25 23:44:27 -0400

    Added functional NearbyPets screen, which displays nearby lost pet and found pet reports.

--- app/src/main/java/petlocator/petlocator/Default_Activity.java
+++ app/src/main/java/petlocator/petlocator/Default_Activity.java
@@ -13,8 +13,6 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
--- app/src/main/java/petlocator/petlocator/NearbyPets.java
+++ app/src/main/java/petlocator/petlocator/NearbyPets.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -25,21 +24,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.LatLng;
-
-public class NearbyPets extends AppCompatActivity {
+public class NearbyPets extends Default_Activity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -55,6 +47,31 @@ public class NearbyPets extends AppCompatActivity {
     private NavigationView navigationView;
     private ActionBarDrawerToggle drawerToggle;
     private Toolbar actionBar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    /* These are the dummy entries in the pet list */
+    String[] itemname ={
+            "Boots",
+            "Labby",
+            "Jeeves",
+            "Splingdo",
+            "Flubgus",
+            "Shmort",
+            "Lil Doogie",
+            "Sarp"
+    };
+
+    Integer[] imgid={
+            R.drawable.cat,
+            R.drawable.dog,
+            R.drawable.cat,
+            R.drawable.dog,
+            R.drawable.cat,
+            R.drawable.dog,
+            R.drawable.cat,
+            R.drawable.dog,
+    };
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -73,6 +90,63 @@ public class NearbyPets extends AppCompatActivity {
         actionBar.setTitle("Nearby Pets");
         setSupportActionBar(actionBar);
 
+        /*
+        Assigning view variables to thier respective view in xml
+        by findViewByID method
+         */
+
+        tabLayout = (TabLayout) findViewById(R.id.lost_spotted_tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        /*
+        Creating Adapter and setting that adapter to the viewPager
+        setSupportActionBar method takes the toolbar and sets it as
+        the default action bar thus making the toolbar work like a normal
+        action bar.
+         */
+        setSupportActionBar(actionBar);
+
+        /*
+        TabLayout.newTab() method creates a tab view, Now a Tab view is not the view
+        which is below the tabs, its the tab itself.
+         */
+
+        final TabLayout.Tab lost = tabLayout.newTab();
+        final TabLayout.Tab spotted = tabLayout.newTab();
+
+        /*
+        Setting Title text for our tabs respectively
+         */
+
+        lost.setText("Lost");
+        spotted.setText("Spotted");
+
+        /*
+        Adding the tab view to our tablayout at appropriate positions
+        As I want home at first position I am passing home and 0 as argument to
+        the tablayout and like wise for other tabs as well
+         */
+        tabLayout.addTab(lost, 0);
+        tabLayout.addTab(spotted, 1);
+
+        /*
+        TabTextColor sets the color for the title of the tabs, passing a ColorStateList here makes
+        tab change colors in different situations such as selected, active, inactive etc
+
+        TabIndicatorColor sets the color for the indicator below the tabs
+         */
+
+        tabLayout.setTabTextColors(ContextCompat.getColorStateList(this, R.drawable.tab_selector));
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.indicator));
+
+        /*
+        Adding a onPageChangeListener to the viewPager
+        1st we add the PageChangeListener and pass a TabLayoutPageChangeListener so that Tabs Selection
+        changes when a viewpager page changes.
+         */
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
         /* The entire section below involving menus and action bars can (and should) be used
            in all activities. */
         menuOptions = getResources().getStringArray(R.array.menu_options);
@@ -83,23 +157,58 @@ public class NearbyPets extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.lost_spotted_tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /* initializes the seekbar which determines radius of search */
+        SeekBar distance_seekbar = (SeekBar) findViewById(R.id.distance_seekbar);
+        final TextView distance_text = (TextView) findViewById(R.id.distance_text);
+
+        distance_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int distance = 5;
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onProgressChanged(SeekBar seekBar, int distanceValue, boolean fromUser) {
+                distance = distanceValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                distance_text.setText("Covered: " + distance + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /* Handles displaying custom entries on an image+text ListView */
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        ListView list = (ListView) findViewById(R.id.nearby_pet_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String selectedItem= itemname[+position];
+                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_SHORT).show();
+                Intent view_lost_report = new Intent(NearbyPets.this, ViewLostReport.class);
+                NearbyPets.this.startActivity(view_lost_report);
+
+
             }
         });
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -214,22 +323,22 @@ public class NearbyPets extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class LostFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public LostFragment() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static LostFragment newInstance(int sectionNumber) {
+            LostFragment fragment = new LostFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
@@ -237,9 +346,10 @@ public class NearbyPets extends AppCompatActivity {
         }
 
         @Override
+        /* This really doesn't work right now! */
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home_page, container, false);
+            View rootView = inflater.inflate(R.layout.nearby_pets_tabs, container, false);
             TableRow firstRow = (TableRow) rootView.findViewById(R.id.row1);
             TableRow secondRow = (TableRow) rootView.findViewById(R.id.row2);
             TextView row1Col1 = (TextView) rootView.findViewById(R.id.row1_col1);
@@ -251,6 +361,7 @@ public class NearbyPets extends AppCompatActivity {
 //            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             switch(getArguments().getInt(ARG_SECTION_NUMBER)) {
                 case 1:
+                    /*
                     row1Col1.setText("Fido");
                     row1Col2.setText("Greyhound");
                     row1Col3.setText("0.1 mi");
@@ -274,9 +385,9 @@ public class NearbyPets extends AppCompatActivity {
                             startActivity(fidoIntent);
                             return true;
                         }
-                    });
+                    }); */
                     break;
-                case 2:
+                case 2: /*
                     row1Col1.setText("Iko");
                     row1Col2.setText("Black lab");
                     row1Col3.setText("1.17 mi");
@@ -300,7 +411,7 @@ public class NearbyPets extends AppCompatActivity {
                             startActivity(firstIntent);
                             return true;
                         }
-                    });
+                    }); */
                 default:
                     break;
             }
@@ -322,7 +433,7 @@ public class NearbyPets extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return LostFragment.newInstance(position + 1);
         }
 
         @Override
@@ -337,7 +448,7 @@ public class NearbyPets extends AppCompatActivity {
                 case 0:
                     return "LOST PETS";
                 case 1:
-                    return "FOUND PETS";
+                    return "SPOTTED PETS";
             }
             return null;
         }
