3a34fb466f530c9cd9e24477d50cfba0b5355fcc
tmfulton <tfulto@gmail.com>
2016-04-27 20:37:29 -0400

    Added report a lost and report a found pet. Still minor bug in getting google map to work

--- app/src/main/java/petlocator/petlocator/ReportLostPet.java
+++ app/src/main/java/petlocator/petlocator/ReportLostPet.java
@@ -12,7 +12,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
 public class ReportLostPet extends AppCompatActivity {
 
@@ -21,6 +30,8 @@ public class ReportLostPet extends AppCompatActivity {
     private NavigationView navigationView;
     private ActionBarDrawerToggle drawerToggle;
     private Toolbar actionBar;
+    private MapView mapView;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,35 @@ public class ReportLostPet extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.left_drawer);
         setUpNavigationView();
         setUpDrawerLayout();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.animal_array, R.layout.timspinner);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        map = mapView.getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        try {
+            map.setMyLocationEnabled(true);
+        } catch (SecurityException e) {
+            // TODO: do something
+        }
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        MapsInitializer.initialize(this.getApplicationContext());
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        map.animateCamera(cameraUpdate);
     }
 
     /**
